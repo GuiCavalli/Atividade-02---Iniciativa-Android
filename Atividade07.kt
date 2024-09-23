85dad31ed6b09625d24d92f5ea100eca36af1def
GuiCavalli <160363168+GuiCavalli@users.noreply.github.com>
2024-09-23 20:18:26 -0300

    Atividade07.kt

--- Atividade07.kt
+++ Atividade07.kt
@@ -4,38 +4,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private var randomNumber: Int = 0
-
+    private val numeroAleatorio = Random.nextInt(1, 101)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        randomNumber = Random.nextInt(1, 101)
-
-        val guessEditText: EditText = findViewById(R.id.guessEditText)
-        val checkButton: Button = findViewById(R.id.checkButton)
-        val feedbackTextView: TextView = findViewById(R.id.feedbackTextView)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        checkButton.setOnClickListener {
-            val userGuess = guessEditText.text.toString().toIntOrNull()
-            feedbackTextView.text = when {
-                userGuess == null -> "Por favor, insira um número válido."
-                userGuess < randomNumber -> "Seu palpite é muito baixo."
-                userGuess > randomNumber -> "Seu palpite é muito alto."
+        val editTextGuess = findViewById<EditText>(R.id.editTextGuess)
+        val buttonCheck = findViewById<Button>(R.id.buttonCheck)
+        val textViewResult = findViewById<TextView>(R.id.textViewResult)
+        buttonCheck.setOnClickListener {
+            val palpiteUsuario = editTextGuess.text.toString().toIntOrNull()
+            textViewResult.text = when {
+                palpiteUsuario == null -> "Insira um número válido."
+                palpiteUsuario < numeroAleatorio -> "Muito baixo!"
+                palpiteUsuario > numeroAleatorio -> "Muito alto!"
                 else -> "Parabéns! Você acertou!"
             }
         }
@@ -44,47 +29,39 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
 <?xml version="1.0" encoding="utf-8"?>
 <androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:id="@+id/main"
     android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context=".MainActivity">
+    android:layout_height="match_parent">
 
     <EditText
-        android:id="@+id/guessEditText"
+        android:id="@+id/editTextGuess"
         android:layout_width="0dp"
         android:layout_height="wrap_content"
         android:hint="Digite seu palpite"
         android:inputType="number"
-        app:layout_constraintEnd_toEndOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
         app:layout_constraintTop_toTopOf="parent"
-        app:layout_constraintBottom_toTopOf="@+id/checkButton"
-        android:layout_margin="16dp" />
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintEnd_toEndOf="parent" />
 
     <Button
-        android:id="@+id/checkButton"
+        android:id="@+id/buttonCheck"
         android:layout_width="wrap_content"
         android:layout_height="wrap_content"
-        android:text="Verificar Palpite"
-        app:layout_constraintEnd_toEndOf="parent"
+        android:text="Verificar"
+        app:layout_constraintTop_toBottomOf="@id/editTextGuess"
         app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toBottomOf="@+id/guessEditText"
-        app:layout_constraintBottom_toBottomOf="parent"
-        android:layout_margin="16dp"/>
+        app:layout_constraintEnd_toEndOf="parent" />
 
     <TextView
-        android:id="@+id/feedbackTextView"
+        android:id="@+id/textViewResult"
         android:layout_width="wrap_content"
         android:layout_height="wrap_content"
-        android:text=""
-        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintTop_toBottomOf="@id/buttonCheck"
         app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toBottomOf="@+id/checkButton"
-        app:layout_constraintBottom_toBottomOf="parent"
-        android:layout_margin="16dp"/>
+        app:layout_constraintEnd_toEndOf="parent" />
 
 </androidx.constraintlayout.widget.ConstraintLayout>
+
